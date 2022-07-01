1f11624fef901bac9360a7e12181f35e5d77c121
Barnabas Varga <vabarnabas@gmail.com>
2022-07-02 00:36:27 +0200

    JSDoc

--- src/index.tsx
+++ src/index.tsx
@@ -10,13 +10,27 @@ import { ProtectedRouteOptions } from "@roszti-types/common"
 import { TokenRequestOptions } from "@roszti-types/authentication"
 import { ProtectedRouteWithIdOptions } from "@roszti-types/users"
 
+/**
+ * A multipurpose client used for making calls to the ROszTI API universal and easier.
+ * @param baseUrl - The URL needed for the requests.
+ */
 export const useROszTIClient = (baseUrl: string) => {
   if (baseUrl === undefined) throw new Error("Missing argument: Base URL")
 
   //Authentication
+  /**
+   * Generates an Access and Refresh token if the user credentials are correct.
+   * @param options - The options required for the function.
+   * @returns An Access and a Refresh token.
+   */
   const getToken = (options: TokenRequestOptions) => {
     return ROszTIFunctionGetToken(options, baseUrl)
   }
+  /**
+   * Returns the current user based on the provided Bearer token.
+   * @param options - The options required for the function.
+   * @returns The user based on the Bearer token.
+   */
   const getCurrentUser = (options: ProtectedRouteOptions) => {
     return ROszTIFunctionGetCurrentUser(options, baseUrl)
   }
