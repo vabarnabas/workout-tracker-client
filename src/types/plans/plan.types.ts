bc58a0ca51b5c231d4becbd6a194ed4af80b7f43
Barnabas Varga <vabarnabas@gmail.com>
2022-09-06 23:01:43 +0200

    Plan Fix

--- src/types/plans/plan.types.ts
+++ src/types/plans/plan.types.ts
@@ -1,10 +1,12 @@
 import { User } from "types/users"
+import { Workout } from "types/workouts"
 
 export interface Plan {
   id: string
   displayName: string
   createdBy: string
   user?: User
+  workouts: Workout[]
 }
 
 export interface UpdatePlan extends Partial<Plan> {
